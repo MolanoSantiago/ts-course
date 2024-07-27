a9974b1281458e7ab50175350004f4e222b974b0
MolanoSantiago <s.molano0818@gmail.com>
2024-07-26 20:25:54 -0500

    Add tsconfig.ts

--- src/demo.ts
+++ src/demo.ts
@@ -1,24 +1,24 @@
 (async () => {
-  const myCart: { prize: number }[] = [];
-  const products: { prize: number }[] = [];
+  const myCart: { price: number }[] = [];
+  const products: { price: number }[] = [];
   const limit = 2;
 
   async function getProducts() {
-    const rta = await fetch('http://api.escuelajs.co/api/v1/products', {method: 'GET'});
+    const rta = await fetch('https://api.escuelajs.co/api/v1/products', {method: 'GET'});
     const data = await rta.json();
-    products.push(...(data as []));
+    products.push(...(data as { price: number }[]));
   }
 
   function getTotal() {
     let total = 0;
     for (let i = 0; i < products.length; i++) {
-      total += products[i].prize;
+      total += products[i].price;
     }
     return total;
   }
 
-  function addProduct(index) {
-    if (getTotal() <= limit) {
+  function addProduct(index: number) {
+    if (getTotal() < limit) {
       myCart.push(products[index]);
     }
   }
@@ -29,7 +29,7 @@
   addProduct(2);
   const total = getTotal();
   console.log(total);
-  const person = {name: 'Nicolas', lastName: 'Molina'}
+  const person = {name: 'Nicolas', lastName: 'Molina'};
   // const rta = person + limit;
   // console.log(rta);
-});
+})();
