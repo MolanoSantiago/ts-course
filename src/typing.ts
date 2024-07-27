d13b877e87cc3e02df3479fe44d3fe5fb6e3fbda
MolanoSantiago <s.molano0818@gmail.com>
2024-07-27 17:41:38 -0500

    Add typed variables

---
+++ src/typing.ts
@@ -0,0 +1,11 @@
+(() => {
+  // Inferred type: number
+  let price = 100;
+// Inferred type: string
+  let product = 'Café';
+
+// Explicit type: number
+  let priceTyped: number = 200;
+// Explicit type: string
+  let productTyped: string = 'Café';
+})();
