f16d96ddb302f9d53bc3a23668404f99a7b3e8c3
MolanoSantiago <s.molano0818@gmail.com>
2024-07-30 23:31:59 -0500

    Add arrays

---
+++ src/array.ts
@@ -0,0 +1,20 @@
+(() => {
+  // Array numbers
+  let numbers = [1, 2, 3, 4, 5];
+  console.log(numbers);
+  numbers.push(6);
+  console.log(numbers);
+  // Array strings
+  let strings = ['a', 'b', 'c', 'd', 'e'];
+  console.log(strings);
+  strings.push('f');
+  console.log(strings);
+
+  // Array mixed
+  let mixed = [1, 'a', 2, 'b', 3, 'c'];
+  console.log(mixed);
+  mixed.push(4);
+  console.log(mixed);
+  mixed.push('d');
+  console.log(mixed);
+})();
