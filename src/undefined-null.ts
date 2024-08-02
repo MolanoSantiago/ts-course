346fd7456aa376a92aca1edc73751afc80ca74f5
MolanoSantiago <s.molano0818@gmail.com>
2024-08-01 23:21:41 -0500

    Add undefined and alias

---
+++ src/undefined-null.ts
@@ -0,0 +1,25 @@
+(() => {
+  let myNull: null = null;
+  let myUndefined: undefined = undefined;
+  let myNumber: number | null;
+  myNumber = 10;
+  console.log(myNumber);
+  myNumber = null;
+  console.log(myNumber);
+  let myString: string | undefined;
+  myString = 'Hello';
+  console.log(myString);
+  myString = undefined;
+  console.log(myString);
+
+  function hello(name: string | null): string {
+    if (name === null) {
+      return 'Hello, stranger';
+    } else {
+      return `Hello, ${name}`;
+    }
+  }
+
+  console.log(hello('John Doe'));
+  console.log(hello(null));
+})();
